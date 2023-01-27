f03d7d2575371d19b7b0b19de751ac0b028321fd
Vung Do <vungxd04@gmail.com>
2023-01-27 17:52:40 +0700

    Internationalization

--- peopledb-web/src/main/java/com/vungdo/peopledbweb/PeopledbWebApplication.java
+++ peopledb-web/src/main/java/com/vungdo/peopledbweb/PeopledbWebApplication.java
@@ -2,11 +2,35 @@ package com.vungdo.peopledbweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class PeopledbWebApplication {
+public class PeopledbWebApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(PeopledbWebApplication.class, args);
 	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.getDefault());
+		return slr;
+	}
 
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
